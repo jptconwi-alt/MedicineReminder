1ca38ab47457b921c1ff722554ae66162d4fd110
jptconwi-alt <jptconwi@nemsu.edu.ph>
2025-11-25 10:39:16 +0800

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -29,3 +29,45 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+// Add to sw.js
+self.addEventListener('push', function(event) {
+    if (event.data) {
+        const data = event.data.json();
+        const options = {
+            body: data.body,
+            icon: '/static/icon-192.png',
+            badge: '/static/icon-192.png',
+            tag: 'medicine-reminder',
+            requireInteraction: true,
+            actions: [
+                {
+                    action: 'taken',
+                    title: 'Mark as Taken'
+                },
+                {
+                    action: 'snooze',
+                    title: 'Snooze 10 min'
+                }
+            ]
+        };
+        
+        event.waitUntil(
+            self.registration.showNotification(data.title, options)
+        );
+    }
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    
+    if (event.action === 'taken') {
+        // Mark medicine as taken
+        clients.openWindow('/');
+    } else if (event.action === 'snooze') {
+        // Snooze logic
+        console.log('Snoozed reminder');
+    } else {
+        clients.openWindow('/');
+    }
+});
